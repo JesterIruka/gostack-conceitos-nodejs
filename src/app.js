6ada949d47263445271a92d8cd4065e505c559ad
jesteriruka <jesteriruka@gmail.com>
2020-04-10 10:56:49 -0300

    Padronização do código

--- src/app.js
+++ src/app.js
@@ -7,10 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-function validateUniqueId(req, res, next) {
-  if (isUuid(req.params.id)) {
-    return next();
-  } else return res.status(400).json({error:'Invalid repository id '+req.params.id, params:req.params});
+function validateUniqueId(request, response, next) {
+  if (isUuid(request.params.id))return next();
+  else return response.status(400).json({error:'Invalid repository id '+request.params.id, params:request.params});
 }
 
 const repositories = [];
@@ -44,14 +43,14 @@ app.put("/repositories/:id", validateUniqueId, (request, response) => {
   return response.json(repositories[index]);
 });
 
-app.delete("/repositories/:id", validateUniqueId, (req, res) => {
-  const { id } = req.params;
+app.delete("/repositories/:id", validateUniqueId, (request, response) => {
+  const { id } = request.params;
   const index = repositories.findIndex(r=>r.id===id);
   if (index < 0) {
-    return res.status(404).json({error:'Repository not found'});
+    return response.status(404).json({error:'Repository not found'});
   }
   repositories.splice(index, 1);
-  return res.status(204).send();
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", validateUniqueId, (request, response) => {
